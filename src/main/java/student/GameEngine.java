cf69afad000739fdad85754cf69125a8e5194e93
Serena Gong <sxgong2@illinois.edu>
2022-02-05 16:10:26 -0600

    Added game starter code to GameEngine.java

--- src/main/java/student/GameEngine.java
+++ src/main/java/student/GameEngine.java
@@ -1,4 +1,69 @@
 package student;
 
+import java.util.Locale;
+
 public class GameEngine {
+    private String input;
+
+    public String startGame(String userInput) {
+        input = userInput.toLowerCase();
+        String[] command = input.split(" +");
+
+        if (command[0].equals("go")) {
+            if (command.length == 1) {
+               return "Please specify the direction of movement";
+            }
+            if (command.length > 2) {
+                return "Please specify one direction of movement";
+            }
+            if (command.length == 2) {
+                return "EDIT LATER ok moved here";
+            }
+        }
+        if (command[0].equals("take")) {
+            if (command.length == 1) {
+                return "Please specify the item to take";
+            }
+            if (command.length > 2) {
+                return "Please specify ONE item to take";
+            }
+            if (command.length == 2) {
+                return "EDIT LATER ok item taken";
+            }
+        }
+        if (command[0].equals("drop")) {
+            if (command.length == 1) {
+                return "Please specify item to drop";
+            }
+            if (command.length > 2) {
+                return "Please specify ONE item to drop";
+            }
+            if (command.length == 2) {
+                return "EDIT LATER ok item dropped";
+            }
+        }
+        if (userInput.equals("examine")) {
+            return "INSERT ROOM INFO HERE";
+        }
+        if (userInput.equals("travel history")) {
+            return "INSERT TRAVEL HISTORY";
+        }
+        if (userInput.equals("inventory")) {
+            return "INVENTORY";
+        }
+        if (userInput.equals("info")) {
+            return "INFO";
+        }
+        if (userInput.equals("exit") || userInput.equals("quit")) {
+            return "Thanks for playing!";
+        }
+        else {
+            return invalidCommand(userInput);
+        }
+    }
+
+    public String invalidCommand(String userInput) {
+        return "'" + userInput.trim() + "' is not a valid command!";
+    }
+
 }
