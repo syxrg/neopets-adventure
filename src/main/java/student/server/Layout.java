5e2fd16cdd36dce1e9b536d6102afa17cf3e619f
Serena Gong <sxgong2@illinois.edu>
2022-02-08 11:26:49 -0600

    Created new java class room for rooms json file

--- src/main/java/student/server/Layout.java
+++ src/main/java/student/server/Layout.java
@@ -1,4 +1,14 @@
 package student.server;
 
 public class Layout {
+    private String startRoom;
+    private String endRoom;
+    private Room[] rooms;
+
+    public Layout(String startRoomInput, String endRoomInput, Room[] roomsInput) {
+        startRoom = startRoomInput;
+        endRoom = endRoomInput;
+        rooms = roomsInput;
+    }
+
 }
