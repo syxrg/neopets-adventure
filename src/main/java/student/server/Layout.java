43a728f0c8c89bca21218759e5a33628f0cfa8c6
Serena Gong <sxgong2@illinois.edu>
2022-02-08 11:41:50 -0600

    Updated Layout.java functions

--- src/main/java/student/server/Layout.java
+++ src/main/java/student/server/Layout.java
@@ -11,4 +11,35 @@ public class Layout {
         rooms = roomsInput;
     }
 
+    public String getStartRoom() {
+        return startRoom;
+    }
+
+    public String getEndRoom() {
+        return endRoom;
+    }
+
+    public Room[] getRooms() {
+        return rooms;
+    }
+
+    public Room getRoomStringAsRoomObject(String roomName) {
+        if (roomName == null || roomName.length() == 0) {
+            throw new IllegalArgumentException("Name of room is invalid");
+        }
+
+        boolean roomFound = false;
+        Room roomLookingFor = new Room ("", null, null, null);
+
+        for (Room roomBeingLookedFor : rooms) {
+            if (roomName.equals(roomBeingLookedFor.getRoomName())) {
+                roomLookingFor = roomBeingLookedFor;
+                roomFound = true;
+            }
+        }
+        if (!(roomFound)) {
+            throw new IllegalArgumentException("Room does not exist");
+        }
+        return roomLookingFor;
+    }
 }
