c133db40631b85c0c0cb0424bf6aa5bf5a38266c
Serena Gong <sxgong2@illinois.edu>
2022-02-08 11:42:03 -0600

    Updated Room.java with functions/methods

--- src/main/java/student/server/Room.java
+++ src/main/java/student/server/Room.java
@@ -1,4 +1,28 @@
 package student.server;
 
 public class Room {
+    private String name;
+    private String description;
+    private String item;
+    private Direction[] directions;
+
+    public Room (String nameInput, String descriptionInput, String itemInput,
+                 Direction[] directionsInput) {
+        name = nameInput;
+        description = descriptionInput;
+        item = itemInput;
+        directions = directionsInput;
+    }
+
+    public String getRoomName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Direction[] getDirections() {
+        return directions;
+    }
 }
