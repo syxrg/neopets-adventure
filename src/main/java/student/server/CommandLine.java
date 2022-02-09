9604ad9c18aa3968c2ca53f31830143cfd32b5a6
Serena Gong <sxgong2@illinois.edu>
2022-02-08 23:55:28 -0600

    Updated CommandLine to start Game

--- src/main/java/student/server/CommandLine.java
+++ src/main/java/student/server/CommandLine.java
@@ -10,13 +10,12 @@ public class CommandLine {
     private final String GAME_WON = "\n" + "Congratulations you've won the game!";
     private final String GAME_EXIT = "Thanks for playing!";
 
-    public CommandLine() throws Exception {
+    public CommandLine() {
        game = new GameEngine();
        scanner = new Scanner(System.in);
     }
 
-    public void gamePlay() throws Exception {
-
+    public void gamePlay() {
         System.out.println("Welcome to the Monstro City Map Game!");
         System.out.println(game.getStartUpInformation());
         System.out.println(game.getCurrentRoomInformation() + game.getItemInRoomInformation());
@@ -24,7 +23,7 @@ public class CommandLine {
         boolean gameIsRunning = true;
         while (gameIsRunning) {
 
-            System.out.print("> ");
+            System.out.print("> "); // shows user where to type, after ">"
 
             String input = scanner.nextLine().trim();
             String output = game.startGame(input);
