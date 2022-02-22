bb379c37fc8a18762a52c4ce226a730d9b107114
Serena Gong <sxgong2@illinois.edu>
2022-02-21 19:28:57 -0600

    Updated @Get in AdventureResource

--- src/main/java/student/server/AdventureResource.java
+++ src/main/java/student/server/AdventureResource.java
@@ -18,8 +18,7 @@ public class AdventureResource {
     @GET
     @Path("ping")
     public String ping() {
-        // TODO: This method should return `pong`.
-        return "";
+        return "pong";
     }
 
     /**
