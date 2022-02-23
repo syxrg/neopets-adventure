43a35be5ba0bac3bc5099a458dc9c3f03573f288
Serena Gong <sxgong2@illinois.edu>
2022-02-22 23:23:04 -0600

    Added API functionality to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
+import org.glassfish.grizzly.http.server.HttpServer;
+import student.server.AdventureResource;
+import student.server.AdventureServer;
 import student.server.CommandLine;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        CommandLine game = new CommandLine();
-        game.gamePlay();
+        //CommandLine game = new CommandLine();
+        //game.gamePlay();
+
+        //api
+        HttpServer server = AdventureServer.createServer(AdventureResource.class);
+        server.start();
     }
 }
