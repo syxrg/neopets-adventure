0eaa86fa3a9da082d69fd4b660a2d83be3c8ee52
Serena Gong <sxgong2@illinois.edu>
2022-02-22 23:23:54 -0600

    Updated method naming in MonstroCityService

--- src/main/java/student/server/MonstroCityService.java
+++ src/main/java/student/server/MonstroCityService.java
@@ -2,24 +2,26 @@ package student.server;
 
 import student.GameEngine;
 
+import java.util.List;
+import java.util.Map;
 import java.util.HashMap;
 import java.util.SortedMap;
 
 public class MonstroCityService implements AdventureService {
     private HashMap<Integer, GameEngine> gamesRunning = new HashMap<>();
-    private int gameId = 0;
+    private int id = 0;
 
 
     @Override
     public void reset() {
-       gameId = 0;
+       id = 0;
        gamesRunning.clear();
     }
 
     @Override
-    public int newGame() throws AdventureException {
-        gamesRunning.put(gameId, new GameEngine());
-        return gameId++;
+    public int newGame() throws Exception {
+        gamesRunning.put(id, new GameEngine());
+        return id++;
     }
 
     @Override
@@ -27,16 +29,32 @@ public class MonstroCityService implements AdventureService {
         GameEngine game = gamesRunning.get(id);
         String image = game.getCurrentRoom().getImage();
 
-        return null;
+        Map<String, List<String>> commandOptions;
+        if (game.gameIsWon()) {
+            commandOptions = new HashMap<>();
+            return new GameStatus(false, id, game.getWinningMessage(),
+                    image, null, new AdventureState(), commandOptions);
+        } else {
+            commandOptions = new HashMap<>();
+            commandOptions.put("go", game.getDirectionMap().get(game.getCurrentRoom().getRoomName()));
+            commandOptions.put("take", game.getItemMap().get(game.getCurrentRoom().getRoomName()));
+            commandOptions.put("drop", game.getItemsPickedUp());
+            commandOptions.put("traversed", game.getRoomsTraversedMap());
+            return new GameStatus(false, id, game.getCurrentRoomInformation() + game.getItemInRoomInfo()
+            + "\n" + game.getInventory(), image, null, new AdventureState(), commandOptions);
+        }
     }
 
     @Override
     public boolean destroyGame(int id) {
-        return false;
+        return gamesRunning.remove(id) != null;
     }
 
     @Override
     public void executeCommand(int id, Command command) {
+        String commandName = command.getCommandName();
+        String commandValue = command.getCommandValue();
+        gamesRunning.get(id).startGame(commandName + "" + commandValue);
 
     }
 
